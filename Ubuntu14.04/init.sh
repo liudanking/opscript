9231447e0baa2a37caada83aac042009a40ef989
liudanking <liudanking@gmail.com>
2015-10-30 11:53:15 +0800

    add ubuntu

--- Ubuntu14.04/init.sh
+++ Ubuntu14.04/init.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # color terminal
-wget -O ~/.bashrc https://raw.githubusercontent.com/liudanking/opscript/master/Debian8.1/bachrc
+wget -O ~/.bashrc https://raw.githubusercontent.com/liudanking/opscript/master/Ubuntu14.04/bachrc
 source ~/.bachrc
 
 
 # install docker
-apt-get update
-apt-get install apt-transport-https -y
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-engine -y
 
